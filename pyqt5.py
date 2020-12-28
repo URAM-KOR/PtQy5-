618ae844115133f81fd4af4b2bcfdb57205820d0
URAM-KOR <76554431+URAM-KOR@users.noreply.github.com>
2020-12-28 10:57:19 +0900

    Update pyqt5.py
    메뉴 그룹 생성, 메뉴 객체 생성, 서브 그룹 ,서브메뉴추가, 트리거 생성

--- pyqt5.py
+++ pyqt5.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
-class Exam(QWidget):
+class Exam(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -10,7 +10,7 @@ class Exam(QWidget):
     def initUI(self):
         btn = QPushButton("눌러", self)
         btn.resize(btn.sizeHint())
-        btn.setToolTip("툴팁:기능없음")
+        btn.setToolTip("기능없음")
         btn.move(25, 30)
         btn.clicked.connect(QCoreApplication.instance().quit)
 
@@ -18,6 +18,32 @@ class Exam(QWidget):
         self.resize(500, 500)
         self.setGeometry(300, 300, 400, 500)
         self.setWindowTitle('첫번째 학습')
+
+        self.statusBar()  # 상태표시줄 생성
+        self.statusBar().showMessage("상태표시줄")
+
+        menu = self.menuBar()              #메뉴 생성
+        menu_file = menu.addMenu('File')    #그룹 생성
+        menu_edit = menu.addMenu('Edit')
+
+        file_exit = QAction('Exit', self)    #메뉴 객체 생성
+        file_exit.setShortcut('Ctrl+Q')
+        file_exit.setStatusTip('누르면 죵료')
+        new_txt = QAction("텍스트 파일",self)
+        new_py = QAction("파이썬 파일",self)
+
+        file_exit.triggered.connect(QCoreApplication.instance().quit)
+
+
+        file_new = QMenu('New', self)  #서브 그룹
+
+        file_new.addAction(new_txt)     #서브 메뉴추가
+        file_new.addAction(new_py)
+
+        menu_file.addMenu(file_new)  # 메뉴 등록 (주 메뉴 추가)
+        menu_file.addAction(file_exit)  #메뉴 등록
+
+
         self.show()
 
     def closeEvent(self, QCloseEvent):
