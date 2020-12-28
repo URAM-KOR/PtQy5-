d62be0273a306ab2425485d4001924f5ad0327dc
URAM-KOR <76554431+URAM-KOR@users.noreply.github.com>
2020-12-28 11:26:17 +0900

    우클릭 메뉴 생성, 실행

--- pyqt5.py
+++ pyqt5.py
@@ -12,7 +12,7 @@ class Exam(QMainWindow):
         btn.resize(btn.sizeHint())
         btn.setToolTip("기능없음")
         btn.move(25, 30)
-        btn.clicked.connect(QCoreApplication.instance().quit)
+        btn.clicked.connect(qApp.quit)
 
 
         self.resize(500, 500)
@@ -24,15 +24,20 @@ class Exam(QMainWindow):
 
         menu = self.menuBar()              #메뉴 생성
         menu_file = menu.addMenu('File')    #그룹 생성
-        menu_edit = menu.addMenu('Edit')
+        menu_edit = menu.addMenu('Edit')    #그룹 생성
+        menu_view = menu.addMenu('View')    #그룹 생성
 
         file_exit = QAction('Exit', self)    #메뉴 객체 생성
         file_exit.setShortcut('Ctrl+Q')
         file_exit.setStatusTip('누르면 죵료')
         new_txt = QAction("텍스트 파일",self)
         new_py = QAction("파이썬 파일",self)
+        view_stat = QAction('상태표시줄', self, checkable = True)
+        view_stat.setChecked(True)
 
-        file_exit.triggered.connect(QCoreApplication.instance().quit)
+
+        file_exit.triggered.connect(qApp.quit)
+        view_stat.triggered.connect(self.tglStat)
 
 
         file_new = QMenu('New', self)  #서브 그룹
@@ -42,9 +47,26 @@ class Exam(QMainWindow):
 
         menu_file.addMenu(file_new)  # 메뉴 등록 (주 메뉴 추가)
         menu_file.addAction(file_exit)  #메뉴 등록
+        menu_view.addAction(view_stat)
+
 
 
         self.show()
+
+    def tglStat(self, state):
+        if state:
+            self.statusBar().show()
+        else:
+            self.statusBar().hide()
+
+    def contextMenuEvent(self, QContextMenuEvent): #우클릭 메뉴
+        cm = QMenu(self)
+
+        quit = cm.addAction("Quit")
+
+        action = cm.exec_(self.mapToGlobal(QContextMenuEvent.pos()))
+        if action == quit:
+            qApp.quit()
 
     def closeEvent(self, QCloseEvent):
         ans = QMessageBox.question(self, "종료할거니", " yes 누르면 종료 ",
@@ -58,3 +80,4 @@ class Exam(QMainWindow):
 app = QApplication(sys.argv)
 w = Exam()
 sys.exit(app.exec_())
+
